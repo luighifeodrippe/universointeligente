94e096f0f42ab6d518e264d24e84d7a7a0193b8f
luighifeodrippe <87806024+luighifeodrippe@users.noreply.github.com>
2021-10-04 21:58:12 -0300

    Add files via upload

---
+++ static/_redirects.js
@@ -0,0 +1,4 @@
+# http and https need separate rules if you don’t force_ssl!
+https://fiquesabendo.netlify.app/* https://www.nesseuniverso.com/:splat 301!
+https://fiquesabendo.netlify.app/* http://nesseuniverso.com/:splat 301!
+https://fiquesabendo.netlify.app/* www.nesseuniverso.com/:splat 301!
